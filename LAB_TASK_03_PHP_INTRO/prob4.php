1890fec65adc612c0e7d816537e3416d1cf730ff
unknown <sharikajoba@gmail.com>
2021-06-13 10:52:13 +0600

    prob4

---
+++ LAB_TASK_03_PHP_INTRO/prob4.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$num1=2;
+$num2=3;
+$num3=4;
+
+if($num1>=$num2 && $num1>=$num3)
+{
+echo "$num1";
+}
+else if($num2>=$num1 && $num2 >= $num3)
+{
+echo "$num2";
+}
+else{
+echo "$num3";
+}
+
+
+?>
