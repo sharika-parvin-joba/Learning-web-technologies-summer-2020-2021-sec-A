10f579af3e638c4d864f963f0ef178cbaa62de6d
unknown <sharikajoba@gmail.com>
2021-06-16 21:35:19 +0600

    DOB3

---
+++ LAB_TASK_4_PHP_FORM/TASK_3/C/C.php
@@ -0,0 +1,21 @@
+<html>
+<head></head>
+<body>
+<fieldset>
+<legend>Email</legend>
+<form  method="post">
+
+dd<input type="number" name="dbo" value="<?php echo $_POST['dbo'] ; ?>">/mm<input type="number" name="dbo2" value="<?php  echo $_POST['dbo2'] ; ?>" >/yyy<input type="number" name="dbo3" value="<?php  echo $_POST['dbo3'] ;  ?>" >
+<br>
+<hr>
+<input type="submit" >
+
+
+<form>
+</fieldset>
+
+
+
+
+</body>
+</html>
