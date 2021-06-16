aecae0e48d624009030e6deb5b211d799e997a38
unknown <sharikajoba@gmail.com>
2021-06-16 21:12:43 +0600

    Email2

---
+++ LAB_TASK_4_PHP_FORM/TASK_2/B/B.php
@@ -0,0 +1,25 @@
+<html>
+<head></head>
+<body>
+<fieldset>
+<legend>Email</legend>
+<form action="B.php" method="post">
+
+<input type="email" name="useremail"><br><hr>
+<input type="submit" >
+
+
+<form>
+</fieldset>
+
+<br>
+
+<?php
+
+
+
+echo $_POST['useremail'] ;
+
+?>
+</body>
+</html>
