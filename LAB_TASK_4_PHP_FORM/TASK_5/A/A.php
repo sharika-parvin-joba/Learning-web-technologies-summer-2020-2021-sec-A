f7eecfe41f26b68100646e785d5dd80d95ddafd1
unknown <sharikajoba@gmail.com>
2021-06-16 22:41:07 +0600

    degree1

---
+++ LAB_TASK_4_PHP_FORM/TASK_5/A/A.php
@@ -0,0 +1,23 @@
+<?php
+
+$degree=$_POST['degree'];
+
+if($degree=='ssc')
+{
+echo ("SSC");
+}
+else if($degree=='hsc')
+{
+echo ("HSC");
+}
+else if($degree=='bsc')
+{
+echo ("BSc");
+}
+else if($degree=='msc')
+{
+echo ("MSc");
+}
+
+
+?>
