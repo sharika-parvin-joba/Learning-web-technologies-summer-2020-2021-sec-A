9b3de0e785ccb1fdb36ac1a7f5d0fc0c34633e3b
unknown <sharikajoba@gmail.com>
2021-06-16 22:25:18 +0600

    GENDER3

---
+++ LAB_TASK_4_PHP_FORM/TASK_4/C/C.php
@@ -0,0 +1,24 @@
+<html>
+<head></head>
+<body>
+<fieldset>
+<legend>GENDER</legend>
+<form  method="post">
+
+<input type="radio" name="gender" value="<?php echo $_POST['gender'] ; ?>">Male
+<input type="radio" name="gender" value="<?php echo $_POST['gender'] ; ?>">Female
+<input type="radio" name="gender" value="<?php echo $_POST['gender'] ; ?>">Others
+
+<br><hr>
+
+<input type="submit" >
+
+
+<form>
+</fieldset>
+
+
+
+
+</body>
+</html>
