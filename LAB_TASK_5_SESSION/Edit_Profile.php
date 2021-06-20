baf0f661e696bf363fc6ec14f4824b4639c85420
unknown <sharikajoba@gmail.com>
2021-06-20 23:36:33 +0600

    pages

---
+++ LAB_TASK_5_SESSION/Edit_Profile.php
@@ -0,0 +1,71 @@
+<html>
+
+<head></head>
+<body>
+<table border="1" height="500" width=700 >
+ <thead align="right">
+<tr>
+<th colspan="2">
+<img src="pic1.jpg" height="100px" width="150px" align="Left" >
+Logged in as<a href=""  > Bob</a>|
+<a href="" >Logout</a>|
+
+</th>
+</tr>
+</thead><br>
+
+<tbody align="left">
+<tr>
+<th>
+<b>Account</b><hr><br>
+<ul>
+<li>
+<a href="">Dashbox</a></li><br>
+<li><a href="">View Profile</a></li><br>
+<li><a href="">Edit Profile</a></li><br>
+<li><a href="">Change Profile Picture</a></li><br>
+<li><a href="">Change Password</a></li><br>
+<li><a href="">Logout</a></li><br>
+
+
+</ul>
+</th>
+
+
+<th width=70%>
+
+<form>
+<fieldset>
+<legend><b>Edit Profile</b></legend>
+
+Name:<input type="text" name="name"><br><hr>
+Email:<input type="email" name="email"><br><hr>
+Gender:<input type="radio" name="gender">Male
+          <input type="radio" name="gender">Female
+          <input type="radio" name="gender">Others
+<br><hr>
+
+Date of Birth:<input type="date" name="dbo"><br><hr>
+
+<input type="submit" name="submit" value="Submit">
+
+</fieldset>
+</form>
+</th>
+
+
+</tr>
+</tbody><br>
+
+<tfoot >
+<tr>
+<th colspan="2">
+Copyright &copy 2017
+</th>
+</tr>
+</tfoot>
+
+</table>
+</body>
+
+</html>
---
+++ LAB_TASK_5_SESSION/Profile_Picture.php
@@ -0,0 +1,62 @@
+<html>
+
+<head></head>
+<body>
+<table border="1" height="500" width=700 >
+ <thead align="right">
+<tr>
+<th colspan="2">
+<img src="pic1.jpg" height="100px" width="150px" align="Left" >
+Logged in as<a href=""  > Bob</a>|
+<a href="" >Logout</a>|
+
+</th>
+</tr>
+</thead><br>
+
+<tbody align="left">
+<tr>
+<th>
+<b>Account</b><hr><br>
+<ul>
+<li>
+<a href="">Dashbox</a></li><br>
+<li><a href="">View Profile</a></li><br>
+<li><a href="">Edit Profile</a></li><br>
+<li><a href="">Change Profile Picture</a></li><br>
+<li><a href="">Change Password</a></li><br>
+<li><a href="">Logout</a></li><br>
+
+
+</ul>
+</th>
+
+
+<th width=70%>
+
+
+<form>
+<fieldset>
+<legend><b>Profile Picture</b></legend>
+<img src="pic2.png"><br>
+<input type="file" name="myfile"><br><hr>
+<input type="submit" name="submit" value="Submit">
+
+</th>
+
+
+</tr>
+</tbody><br>
+
+<tfoot >
+<tr>
+<th colspan="2">
+Copyright &copy 2017
+</th>
+</tr>
+</tfoot>
+
+</table>
+</body>
+
+</html>
